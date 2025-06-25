9ecb3df0fb40670a2d1dc4a512f5085ee80570b4
lozhang <lozhang@noreply.gitcode.com>
2025-06-25 14:20:33 +0800

    chore: 统一字符串引号风格，调整 middleware.py 文件中的请求 ID 处理逻辑

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -6,13 +6,13 @@ from starlette.requests import Request
 
 from app.core.log import logger
 
-REQUEST_ID_CTX_KEY = 'request_id'
+REQUEST_ID_CTX_KEY = "request_id"
 _request_id_ctx_var: ContextVar[str] = ContextVar(REQUEST_ID_CTX_KEY, default=None)
 # print('top id is', id(_request_id_ctx_var))
 #
 
-class RequestContextLogMiddleware(BaseHTTPMiddleware):
 
+class RequestContextLogMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint):
         request_id = _request_id_ctx_var.set(str(uuid4()))
         # print('set id is', id(_request_id_ctx_var))
@@ -28,15 +28,17 @@ class RequestContextLogMiddleware(BaseHTTPMiddleware):
         response = await call_next(request)
 
         logger.info("request finished (in middleware)")
-        response.headers['X-Request-ID'] = get_request_id()
+        response.headers["X-Request-ID"] = get_request_id()
 
         _request_id_ctx_var.reset(request_id)
 
         return response
 
+
 def get_request_id() -> str:
     # print('get id is', id(_request_id_ctx_var))
     return _request_id_ctx_var.get()
+
 
 def patch_log(record):
     record["extra"]["request_id"] = get_request_id()
